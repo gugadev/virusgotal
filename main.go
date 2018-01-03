8570563c47ca766ec5aa22e58e76b711397eccc7
Gustavo García <gugadev@MacbookPro.local>
2018-01-02 21:36:08 -0500

    added main file

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/common-nighthawk/go-figure"
+
+	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
+
+	"github.com/gugadev/virusgotal/requests"
+	"github.com/gugadev/virusgotal/utils"
+)
+
+func main() {
+	var FileUtils utils.Files
+	var Console utils.Console
+
+	// console outs
+	green := color.New(color.FgGreen).Add(color.Bold)
+	white := color.New(color.FgWhite).Add(color.Bold)
+	whiteUnderline := color.New(color.FgWhite).Add(color.Bold, color.Underline)
+	ascii := figure.NewFigure("VirusGotal", "graffiti", false)
+
+	// spin
+	spin := spinner.New(spinner.CharSets[39], 200*time.Millisecond)
+
+	// arguments
+	key := flag.String("key", "Your VT API Key", "Ex.: dg2nfng304ngdjng234fng4tfnfjn34")
+	path := flag.String("file", "Path of the file", "/home/janedoe/suspectfile.ext")
+	flag.Parse()
+
+	// open the file to send to VT
+	file := FileUtils.Open(*path)
+	fileStat, _ := file.Stat()
+
+	Console.Clear()
+
+	ascii.Print()
+
+	green.Print("\nScanning file ")
+	white.Printf("%s ", fileStat.Name())
+	spin.Start()
+
+	// upload and scan the file
+	scan := requests.Upload(*key, file)
+
+	spin.Stop()
+	whiteUnderline.Print("\n\nScan results:\n\n")
+
+	// get the report
+	report := requests.GetReport(*key, scan.Resource)
+
+	// show the report into a table
+	report.Show()
+}
